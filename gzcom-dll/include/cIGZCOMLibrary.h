2fd4aec2714b882c5297d2ec075ce1386d768276
nsgomez <nelson.gomez.msd@gmail.com>
2016-05-16 12:09:38 -0400

    Fix GZCOM/GZCOMLibrary interfaces to match Mac vtables

--- gzcom-dll/include/cIGZCOMLibrary.h
+++ gzcom-dll/include/cIGZCOMLibrary.h
@@ -4,17 +4,13 @@
 class cIGZCOMDirector;
 class cIGZString;
 
-// Mostly guessed as to what this interface includes.
+// Vtable ordering from Mac version
 class cIGZCOMLibrary : public cIGZUnknown
 {
 	public:
-		virtual bool Load(void) = 0;
-		virtual bool Free(void) = 0;
-		virtual bool IsLoaded(void) const = 0;
-		virtual void SetLoaded(bool bUse) = 0;
-		/*virtual bool GetPath(cIGZString& szCopyTo) = 0;
+		virtual bool GetPath(cIGZString& szCopyTo) = 0;
 		virtual cIGZCOMDirector* GetDirector(void) = 0;
-		//virtual int operator<(cIGZCOMLibrary const&) = 0;
-		virtual bool IsLoaded(void) = 0;
-		virtual uintptr_t GetHandle(void) = 0;*/
+
+		virtual uintptr_t GetHandle(void) = 0;
+		virtual bool IsLoaded(void) const = 0;
 };
