d096ce2ee27c5ad558d867a8dacb16cee2df106a
nsgomez <nelson.gomez.msd@gmail.com>
2016-05-27 17:17:08 -0400

    Update cGZExtraCheatsDllDirector to demonstrate message reception

--- gzcom-dll/src/cGZExtraCheatsDllDirector.cpp
+++ gzcom-dll/src/cGZExtraCheatsDllDirector.cpp
@@ -1,16 +1,46 @@
 #include "../include/cIGZApp.h"
 #include "../include/cIGZFrameWork.h"
+#include "../include/cIGZMessage2.h"
+#include "../include/cIGZMessageServer2.h"
+#include "../include/cIGZMessageTarget2.h"
 #include "../include/cISC4App.h"
+#include "../include/cISC4RenderProperties.h"
 #include "../include/cRZCOMDllDirector.h"
+#include "../include/GZServPtrs.h"
+#include <Windows.h>
 
 static const uint32_t kExtraCheatsPluginCOMDirectorID = 0x8bbd9623;
 static const uint32_t kGZIID_cISC4App = 0x26ce01c0;
 
-class cSC4ExtraCheatsPluginCOMDirector : public cRZCOMDllDirector
+class cSC4ExtraCheatsPluginCOMDirector : public cRZCOMDllDirector, public cIGZMessageTarget2
 {
 	public:
+		/* Failing to explicitly delegate these methods results in some
+		   ambiguity when resolving which virtual methods to use: those
+		   from cIGZMessageTarget (which are pure abstract) or those from
+		   cRZCOMDllDirector, and the compiler will complain. */
+		bool QueryInterface(uint32_t riid, void** ppvObj) {
+			return cRZCOMDllDirector::QueryInterface(riid, ppvObj);
+		}
+
+		uint32_t AddRef(void) {
+			return cRZCOMDllDirector::AddRef();
+		}
+
+		uint32_t Release(void) {
+			return cRZCOMDllDirector::Release();
+		}
+
 		uint32_t GetDirectorID() const {
 			return kExtraCheatsPluginCOMDirectorID;
+		}
+
+		bool DoMessage(cIGZMessage2* pMessage) {
+			if (pMessage->GetType() == 0xea8ae29a) {
+				MessageBoxA(NULL, "City finished loading", NULL, NULL);
+			}
+
+			return true;
 		}
 
 		bool PreAppInit() {
@@ -23,6 +53,17 @@ class cSC4ExtraCheatsPluginCOMDirector : public cRZCOMDllDirector
 						pISC4App->SetDebugFunctionalityEnabled(true);
 					}
 				}
+			}
+
+			return true;
+		}
+
+		bool PostAppInit() {
+			// The cRZSysServPtr will automatically try to receive the service
+			// pointer for us.
+			cIGZMessageServer2Ptr pMsgServ;
+			if (pMsgServ) {
+				pMsgServ->AddNotification(this, 0xea8ae29a);
 			}
 
 			return true;
