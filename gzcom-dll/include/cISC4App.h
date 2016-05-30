221e0ff4f55fb8433d6cb5d0f0adc073ff5e83e9
nsgomez <nelson.gomez.msd@gmail.com>
2016-05-30 02:32:03 -0400

    Fix SC4App interface not matching the vtable

--- gzcom-dll/include/cISC4App.h
+++ gzcom-dll/include/cISC4App.h
@@ -17,9 +17,10 @@ class cISC4RenderProperties;
 class cISC4App : public cIGZUnknown
 {
 	public:
-		virtual bool EnableFullGamePauseOnAppFocusLoss(bool bEnable) = 0;
-		virtual bool FullGamePause(bool bPause) = 0;
-		virtual bool GainLoseFocus(bool bFocus) = 0;
+		virtual bool OnIdle(void) = 0;
+
+		virtual bool RunMessageServerPump(uint32_t dwMinMessages, uint32_t dwMaxMessages, uint32_t dwMaxTime) = 0;
+		virtual bool RunMessageServer2Pump(uint32_t dwMinMessages, uint32_t dwMaxMessages, uint32_t dwMaxTime) = 0;
 		
 		virtual bool RequestNewCity(intptr_t pCity) = 0; // Actually an AutoRefCount<cISC4RegionalCity>
 		virtual bool RequestLoadCity(void) = 0;
@@ -29,12 +30,13 @@ class cISC4App : public cIGZUnknown
 		virtual bool RequestQuitFromRegion(bool bShowDialog) = 0;
 		virtual bool RequestGoToRegionView(bool bShowDialog) = 0;
 		
-		virtual bool NewCity(void) = 0;
 		virtual bool LoadCity(cIGZString& szString, intptr_t pCityOut) = 0; // Actually an AutoRefCount<cISC4RegionalCity>
 		virtual bool CloseCity(void) = 0;
 		virtual bool SaveCity(bool bUnknown) = 0;
 		virtual bool SaveCity(cIGZString const& szName, bool bUnknown) = 0;
-		virtual bool Quit(void) = 0;
+		
+		virtual bool SavePreferences(void) = 0;
+		virtual bool EnableFullGamePauseOnAppFocusLoss(bool bEnable) = 0;
 
 		virtual bool ApplyVideoPreferences(intptr_t const sPreferences) = 0; // Actually takes SC4VideoPreferences const&
 		virtual bool GetAutoVideoPreferences(intptr_t pPreferencesOut) = 0; // Actually takes SC4VideoPreferences&
@@ -48,8 +50,14 @@ class cISC4App : public cIGZUnknown
 		// All intptr_t types here are actually unspecified class types that have
 		// been omitted for simplicity's sake.
 		virtual int32_t GetAppState(void) = 0;
-		virtual bool GetAppName(cIGZString& szNameOut) = 0;
 		virtual intptr_t GetMainWindow(void) = 0;
+		virtual bool GetAppName(cIGZString& szNameOut) = 0;
+
+		virtual bool GetAppIniFileName(cIGZString& szPathOut) = 0;
+		virtual bool GetAppIniFilePath(cIGZString& szPathOut) = 0;
+		virtual bool GetAppPreferencesFileName(cIGZString& szPathOut) = 0;
+		virtual bool GetAppPreferencesFilePath(cIGZString& szPathOut) = 0;
+
 		virtual cISC4FeatureManager* GetFeatureManager(void) = 0;
 		virtual cIGZCheatCodeManager* GetCheatCodeManager(void) = 0;
 		virtual cISC4Nation* GetNation(void) = 0;
@@ -84,14 +92,11 @@ class cISC4App : public cIGZUnknown
 		virtual bool GetTempDirectory(cIGZString& szPathOut) = 0;
 		virtual bool GetExceptionReportsDirectory(cIGZString& szPathOut) = 0;
 		virtual bool GetTestScriptDirectory(cIGZString& szPathOut) = 0;
-		virtual bool GetAppIniFileName(cIGZString& szPathOut) = 0;
-		virtual bool GetAppIniFilePath(cIGZString& szPathOut) = 0;
-		virtual bool GetAppPreferencesFileName(cIGZString& szPathOut) = 0;
-		virtual bool GetAppPreferencesFilePath(cIGZString& szPathOut) = 0;
+
+		virtual bool AddDynamicLibraryByName(cIGZString const& sName, cIGZString* pBasePath, bool bIgnoreINI) = 0;
+		virtual bool AddDynamicLibraryByPath(cIGZString const& sPath, bool bIgnoreINI) = 0;
 
 		typedef bool (*ShutdownCallback)(void*);
 		virtual bool RegisterShutdownCallbackFunction(ShutdownCallback pfCallback, void* pUnknown) = 0;
 		virtual bool UnregisterShutdownCallbackFunction(ShutdownCallback pfCallback, void* pUnknown) = 0;
-		
-		virtual bool LoadRegistry(void) = 0;
 };
