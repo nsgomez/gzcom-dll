7e3ba3323458a0430f24ee12a6bf920c1b8ea57c
nsgomez <nelson.gomez.msd@gmail.com>
2016-10-26 18:49:14 -0400

    Add IGZUnknownList

---
+++ gzcom-dll/include/cIGZUnknownList.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "cIGZUnknown.h"
+
+class cIGZUnknownEnumerator;
+
+class cIGZUnknownList : public cIGZUnknown
+{
+	public:
+		virtual size_t GetSize(void) = 0;
+		
+		virtual bool GetElement(uint32_t dwOffset, uint32_t riid, void** ppvObj) = 0;
+		virtual bool EnumElements(cIGZUnknown& sEnum, cIGZUnknown* pUnknown, uint32_t riid) = 0;
+
+		virtual bool Push_Front(cIGZUnknown* pElem) = 0;
+		virtual bool Push_Front(cIGZUnknownList const& sList) = 0;
+
+		virtual bool Push_Back(cIGZUnknown* pElem) = 0;
+		virtual bool Push_Back(cIGZUnknownList const& sList) = 0;
+
+		virtual bool Peek_Front(uint32_t riid, void** ppvObj) = 0;
+		virtual bool Peek_Back(uint32_t riid, void** ppvObj) = 0;
+		virtual bool Pop_Front(uint32_t riid, void** ppvObj) = 0;
+		virtual bool Pop_Back(uint32_t riid, void** ppvObj) = 0;
+
+		virtual bool Clear(void) = 0;
+		virtual bool CopyFrom(cIGZUnknownList const& sList) = 0;
+		virtual bool CreateClone(cIGZUnknownList** ppList) = 0;
+
+		virtual bool ConstEnumElements(cIGZUnknownEnumerator& sEnum, cIGZUnknown* pUnknown, uint32_t riid) = 0;
+
+};
