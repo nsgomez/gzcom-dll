3c27f033f652a413198e39a442ded24afdc3fb8a
nsgomez <nelson.gomez.msd@gmail.com>
2016-11-03 22:18:48 -0400

    Use mouse position for actions in ExtraExtraCheats

--- gzcom-dll/src/cGZExtraExtraCheatsDllDirector.cpp
+++ gzcom-dll/src/cGZExtraExtraCheatsDllDirector.cpp
@@ -5,6 +5,9 @@
 #include "../include/cIGZMessageServer2.h"
 #include "../include/cIGZMessage2Standard.h"
 #include "../include/cIGZMessageTarget2.h"
+#include "../include/cIGZMouseMessage.h"
+#include "../include/cIGZWin.h"
+#include "../include/cIGZWinMgr.h"
 #include "../include/cISC4App.h"
 #include "../include/cISC4City.h"
 #include "../include/cISC4DisasterLayer.h"
@@ -14,9 +17,11 @@
 #include "../include/cISC4Occupant.h"
 #include "../include/cISC4PollutionSimulator.h"
 #include "../include/cISC4SimGrid.h"
+#include "../include/cISC4View3DWin.h"
 #include "../include/cRZAutoRefCount.h"
 #include "../include/cRZBaseString.h"
 #include "../include/cRZCOMDllDirector.h"
+#include "../include/cRZPoint.h"
 #include "../include/cS3DCamera.h"
 #include "../include/cS3DVector3.h"
 #include "../include/GZCLSIDDefs.h"
@@ -29,11 +34,18 @@ static const uint32_t kExtraExtraCheatsPluginCOMDirectorID = 0x5b76862b;
 static const uint32_t kDisasterID_Riot = 0xca380b06;
 static const uint32_t kDisasterID_ToxicSpill = 0xebfb0404;
 static const uint32_t kGZIID_cIGZCheatCodeManager = 0xa1085722;
+static const uint32_t kGZIID_cIGZMouseMessage = 0x1a0c615f;
 static const uint32_t kGZIID_cISC4App = 0x26ce01c0;
 static const uint32_t kGZIID_cISC4DisasterInstance = 0x49b43f58;
 static const uint32_t kGZIID_cISC4LotManager = 0x681905bd;
+static const uint32_t kGZIID_cISC4View3DWin = -95964167;
 static const uint32_t kGZIID_cS3DCamera = 0xe9c6262a;
+
+static const uint32_t kGZWin_WinSC4App = 0x6104489a;
+static const uint32_t kGZWin_SC4View3DWin = 0x9a47b417;
+
 static const uint32_t kGZMSG_CheatIssued = 0x230e27ac;
+static const uint32_t kGZMSG_MouseMessage = 0xfa0c6390;
 
 static const uint32_t kCheatSievertBeGone = 0x9e0c0b4d;
 static const uint32_t kCheatChernobyl = 0x606a36f7;
@@ -84,6 +96,13 @@ class cGZExtraExtraCheatsPluginCOMDirector : public cRZCOMDllDirector, public cI
 
 				ProcessCheat(dwCheatID, pszCheatData);
 			}
+			else if (pMessage->GetType() == kGZMSG_MouseMessage) {
+				cIGZMouseMessage* pMouseMsg;
+				if (pMessage->QueryInterface(kGZIID_cIGZMouseMessage, (void**)&pMouseMsg)) {
+					m_sMousePos = pMouseMsg->Position();
+					pMouseMsg->Release();
+				}
+			}
 
 			return true;
 		}
@@ -105,6 +124,11 @@ class cGZExtraExtraCheatsPluginCOMDirector : public cRZCOMDllDirector, public cI
 						}
 					}
 				}
+			}
+
+			cIGZMessageServer2Ptr pMsgServ;
+			if (pMsgServ) {
+				pMsgServ->AddNotification(this, kGZMSG_MouseMessage);
 			}
 
 			return true;
@@ -133,8 +157,8 @@ class cGZExtraExtraCheatsPluginCOMDirector : public cRZCOMDllDirector, public cI
 			switch (dwCheatID) {
 				case kCheatSievertBeGone: return SetAllRadiation(false);
 				case kCheatChernobyl: return SetAllRadiation(true);
-				case kCheatUncivilDisobediece: return TryToStartDisasterAtRandomLocation(kDisasterID_Riot);
-				case kCheatHazmat: return TryToStartDisasterAtRandomLocation(kDisasterID_ToxicSpill);
+				case kCheatUncivilDisobediece: return TryToStartDisasterAtMouseLocation(kDisasterID_Riot);
+				case kCheatHazmat: return TryToStartDisasterAtMouseLocation(kDisasterID_ToxicSpill);
 				default: return false;
 			}
 		}
@@ -172,21 +196,33 @@ class cGZExtraExtraCheatsPluginCOMDirector : public cRZCOMDllDirector, public cI
 			return true;
 		}
 
-		bool TryToStartDisasterAtRandomLocation(uint32_t dwDisasterType) {
+		bool TryToStartDisasterAtMouseLocation(uint32_t dwDisasterType) {
 			cISC4AppPtr pApp;
 			if (!pApp) return false;
 
 			cISC4City* pCity = pApp->GetCity();
 			if (!pCity) return false;
 
-			int32_t nX, nZ;
-			if (!GetRandomLotLocation(pCity, nX, nZ)) return false;
+			cIGZWin* pParentWin = pApp->GetMainWindow();
+			if (!pParentWin) return false;
+
+			pParentWin = pParentWin->GetChildWindowFromID(kGZWin_WinSC4App);
+			if (!pParentWin) return false;
+
+			cISC4View3DWin* pWin = NULL;
+			if (!pParentWin->GetChildAs(kGZWin_SC4View3DWin, kGZIID_cISC4View3DWin, (void**)&pWin)) return false;
 
 			cISC4DisasterLayer* pDisasterLayer = pCity->GetDisasterLayer();
 			if (!pDisasterLayer) return false;
 
 			cISC4DisasterManager* pDisasterManager = pDisasterLayer->GetDisasterManager(dwDisasterType);
 			if (!pDisasterManager) return false;
+
+			float fPosition[3];
+			int32_t nX, nZ;
+
+			pWin->PickTerrain(m_sMousePos.nX, m_sMousePos.nY, fPosition, false);
+			pCity->PositionToCell(fPosition[0], fPosition[2], nX, nZ);
 
 			pDisasterManager->AttemptDisasterStart(nX, nZ);
 			return true;
@@ -204,6 +240,9 @@ class cGZExtraExtraCheatsPluginCOMDirector : public cRZCOMDllDirector, public cI
 			pLotManager->Release();
 			return true;
 		}
+
+	protected:
+		cRZPoint m_sMousePos;
 };
 
 // You need to replace the director returned here for the game and this DLL
