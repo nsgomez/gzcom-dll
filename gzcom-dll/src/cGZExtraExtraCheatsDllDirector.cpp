9777c953fa0b2eb21255bf9b80c72d3342bda983
nsgomez <nelson.gomez.msd@gmail.com>
2016-12-26 14:05:05 -0500

    Comment out RZGetCOMDllDirector definition
    This is supposed to be user-defined. Remove our own
    definition to make it easier to clone and extend this
    repo.
    
    All sample directors should eventually be moved to their
    own repository. Obviously removing this definition will
    break linkage when trying to compile this repository
    standalone, which isn't desirable but it's fine if this
    repo is extended and another library provides it anyway.

--- gzcom-dll/src/cGZExtraExtraCheatsDllDirector.cpp
+++ gzcom-dll/src/cGZExtraExtraCheatsDllDirector.cpp
@@ -248,7 +248,7 @@ class cGZExtraExtraCheatsPluginCOMDirector : public cRZCOMDllDirector, public cI
 // You need to replace the director returned here for the game and this DLL
 // to use the right director. This is the only place that it's necessary to
 // do so.
-cRZCOMDllDirector* RZGetCOMDllDirector() {
+/*cRZCOMDllDirector* RZGetCOMDllDirector() {
 	static cGZExtraExtraCheatsPluginCOMDirector sDirector;
 	return &sDirector;
-}
+}*/
