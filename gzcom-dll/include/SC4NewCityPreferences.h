55d1cea4d52902eb9065b46150fdbe4c952f00d3
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-05-29 14:49:33 -0600

    Fill in the SC4NewCityPreferences field names

--- gzcom-dll/include/SC4NewCityPreferences.h
+++ gzcom-dll/include/SC4NewCityPreferences.h
@@ -4,8 +4,8 @@
 class SC4NewCityPreferences
 {
 public:
-    uint8_t field0_0x0[512];   // 0x0
-    uint8_t field1_0x200[512]; // 0x200
-    uint32_t field2_0x400;     // 0x400
+    char cityName[512];        // 0x0
+    char mayorName[512];       // 0x200
+    uint32_t difficultyLevel;  // 0x400
 };
 
