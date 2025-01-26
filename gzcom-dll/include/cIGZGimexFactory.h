cf04bbfdfe26479f6319ecb66571dae164f1ff77
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-01-26 06:23:38 -0700

    Revert "Fix the order of a few cIGZGimexFactory methods"
    The original order was correct for calling CreateFromResource.
    
    This reverts commit 799185970434735bb273bf7092e7d6a0b860177e.

--- gzcom-dll/include/cIGZGimexFactory.h
+++ gzcom-dll/include/cIGZGimexFactory.h
@@ -14,15 +14,13 @@ static const uint32_t GZIID_cIGZGimexFactory = 0x02ffbb60;
 
 class cIGZGimexFactory : public cIGZUnknown
 {
-public:	
-	virtual bool LoadFromFile(cIGZFile* pFile, uint32_t gimexIndex, cIGZBuffer* pBuffer, cGZBufferType bufferType) = 0;
-	virtual bool LoadFromFile(cIGZString const& path, cIGZBuffer** outBuffer, cGZBufferType bufferType) = 0;
+public:
 	virtual bool LoadFromFile(cIGZString const& path, cIGZBuffer* pBuffer, cGZBufferType bufferType) = 0;
+	virtual bool LoadFromFile(cIGZString const& path, cIGZBuffer** outBuffer, cGZBufferType bufferType) = 0;
 
-	virtual bool SaveToFile(cIGZFile* pFile, uint32_t gimexIndex, cIGZBuffer* pBuffer, uint32_t unknown1) = 0;
-	virtual bool SaveToSerialRecord(cIGZPersistDBSerialRecord* pRecord, uint32_t gimexIndex, cIGZBuffer* pBuffer, SC4Rect<int32_t>& area, uint32_t unknown1) = 0;
 	virtual bool SaveToFile(cIGZString const& path, cIGZBuffer* pBuffer, SC4Rect<int32_t>& area, uint32_t formatID) = 0;
 	virtual bool SaveToFile(cIGZString const& path, cIGZBuffer* pBuffer, uint32_t formatID) = 0;
+	virtual bool LoadFromFile(cIGZFile* pFile, uint32_t gimexIndex, cIGZBuffer* pBuffer, cGZBufferType bufferType) = 0;
 	virtual bool CreateFromFile(cIGZFile* pFile, uint32_t gimexIndex, cIGZBuffer** outBuffer, cGZBufferType bufferType) = 0;
 
 	virtual bool LoadFromResource(cGZPersistResourceKey& key, uint32_t gimexIndex, cIGZBuffer* pBuffer, cGZBufferType bufferType) = 0;
@@ -30,8 +28,10 @@ public:
 	virtual bool LoadFromSerialRecord(cIGZPersistDBSerialRecord* pRecord, uint32_t gimexIndex, cIGZBuffer* pBuffer, cGZBufferType bufferType) = 0;
 	virtual bool CreateFromSerialRecord(cIGZPersistDBSerialRecord* pRecord, uint32_t gimexIndex, cIGZBuffer** outBuffer, cGZBufferType bufferType) = 0;
 
+	virtual bool SaveToFile(cIGZFile* pFile, uint32_t gimexIndex, cIGZBuffer* pBuffer, SC4Rect<int32_t>& area, uint32_t formatID) = 0;
+	virtual bool SaveToFile(cIGZFile* pFile, uint32_t gimexIndex, cIGZBuffer* pBuffer, uint32_t formatID) = 0;
 	virtual bool SaveToResource(cGZPersistResourceKey& key, cIGZPersistDBSegment* pSegment, uint32_t gimexIndex, cIGZBuffer* pBuffer, SC4Rect<int32_t>& area, uint32_t formatID) = 0;
 	virtual bool SaveToResource(cGZPersistResourceKey& key, cIGZPersistDBSegment* pSegment, uint32_t gimexIndex, cIGZBuffer* pBuffer, uint32_t formatID) = 0;
+	virtual bool SaveToSerialRecord(cIGZPersistDBSerialRecord* pRecord, uint32_t gimexIndex, cIGZBuffer* pBuffer, SC4Rect<int32_t>& area, uint32_t formatID) = 0;
 	virtual bool SaveToSerialRecord(cIGZPersistDBSerialRecord* pRecord, uint32_t gimexIndex, cIGZBuffer* pBuffer, uint32_t formatID) = 0;
-	virtual bool SaveToFile(cIGZFile* pFile, uint32_t gimexIndex, cIGZBuffer* pBuffer, SC4Rect<int32_t>& area, uint32_t unknown1) = 0;
 };
