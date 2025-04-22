88f91102bd2acb0f53f51adfc4ffc4d0c3424a9d
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-04-22 11:32:17 -0600

    Add more SCPropertyUtil::GetPropertyValue overloads

--- gzcom-dll/include/SCPropertyUtil.h
+++ gzcom-dll/include/SCPropertyUtil.h
@@ -2,10 +2,26 @@
 #include "cISCPropertyHolder.h"
 
 class cIGZString;
+struct StringResourceKey;
 
 namespace SCPropertyUtil
 {
-	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint32_t& value);
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int8_t& value);
 	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint8_t& value);
+
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int16_t& value);
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint16_t& value);
+
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int32_t& value);
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint32_t& value);
+
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int64_t& value);
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint64_t& value);
+
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, float& value);
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, double& value);
+
 	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, cIGZString& value);
+
+	bool GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, StringResourceKey& value);
 }
--- gzcom-dll/src/SCPropertyUtil.cpp
+++ gzcom-dll/src/SCPropertyUtil.cpp
@@ -2,8 +2,9 @@
 #include "cISCPropertyHolder.h"
 #include "cISCProperty.h"
 #include "cIGZVariant.h"
+#include "StringResourceKey.h"
 
-bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint32_t& value)
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int8_t& value)
 {
 	if (propertyHolder)
 	{
@@ -15,7 +16,7 @@ bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, 
 
 			if (data)
 			{
-				return data->GetValUint32(value);
+				return data->GetValSint8(value);
 			}
 		}
 	}
@@ -45,6 +46,174 @@ bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, 
 	return false;
 }
 
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int16_t& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValSint16(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint16_t& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValUint16(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int32_t& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValSint32(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint32_t& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValUint32(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, int64_t& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValSint64(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, uint64_t& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValUint64(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, float& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValFloat32(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, double& value)
+{
+	if (propertyHolder)
+	{
+		const cISCProperty* property = propertyHolder->GetProperty(id);
+
+		if (property)
+		{
+			const cIGZVariant* data = property->GetPropertyValue();
+
+			if (data)
+			{
+				return data->GetValFloat64(value);
+			}
+		}
+	}
+
+	value = 0;
+	return false;
+}
+
 bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, cIGZString& value)
 {
 	if (propertyHolder)
@@ -63,5 +232,39 @@ bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, 
 		}
 	}
 
+	return false;
+}
+
+bool SCPropertyUtil::GetPropertyValue(const cISCPropertyHolder* propertyHolder, uint32_t id, StringResourceKey& value)
+{
+	const cISCProperty* pProperty = propertyHolder->GetProperty(id);
+
+	if (pProperty)
+	{
+		const cIGZVariant* pVariant = pProperty->GetPropertyValue();
+
+		if (pVariant)
+		{
+			if (pVariant->GetType() == cIGZVariant::Uint32Array)
+			{
+				if (pVariant->GetCount() == 3)
+				{
+					const uint32_t kStringResourceType = 0x2026960B;
+
+					const uint32_t* pData = pVariant->RefUint32();
+
+					if (pData[0] == kStringResourceType)
+					{
+						value.groupID = pData[1];
+						value.instanceID = pData[2];
+						return true;
+					}
+				}
+			}
+		}
+	}
+
+	value.groupID = 0;
+	value.instanceID = 0;
 	return false;
 }
