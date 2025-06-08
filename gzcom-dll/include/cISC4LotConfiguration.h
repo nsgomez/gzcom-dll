92d15905f2a6cd57586a1f710e45072c8ada4081
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-06-08 02:50:32 -0600

    Mark a bunch of cISC4LotConfiguration methods as const

--- gzcom-dll/include/cISC4LotConfiguration.h
+++ gzcom-dll/include/cISC4LotConfiguration.h
@@ -12,30 +12,30 @@ template <typename T> class SC4Vector;
 class cISC4LotConfiguration : public cIGZUnknown
 {
 	public:
-		virtual uint32_t GetID(void) = 0;
-		virtual bool GetName(cIGZString& szName) = 0;
+		virtual uint32_t GetID(void) const = 0;
+		virtual bool GetName(cIGZString& szName) const = 0;
 
-		virtual bool BelongsToFamily(uint32_t dwFamilyId) = 0;
+		virtual bool BelongsToFamily(uint32_t dwFamilyId) const = 0;
 		virtual SC4Vector<uint32_t>* GetFamilyIDArray(void) = 0;
 
-		virtual bool GetSize(uint32_t& nSizeX, uint32_t& nSizeZ) = 0;
-		virtual float GetMaximumSlope(void) = 0;
-		virtual float GetMinimumSlope(void) = 0;
-		virtual float GetMaximumSlopeBeforeFoundation(void) = 0;
+		virtual bool GetSize(uint32_t& nSizeX, uint32_t& nSizeZ) const = 0;
+		virtual float GetMaximumSlope(void) const = 0;
+		virtual float GetMinimumSlope(void) const = 0;
+		virtual float GetMaximumSlopeBeforeFoundation(void) const = 0;
 
-		virtual uint8_t GetGrowthStage(void) = 0;
-		virtual uint16_t GetMinBuildingCapacity(void) = 0;
-		virtual uint16_t GetMaxBuildingCapacity(void) = 0;
+		virtual uint8_t GetGrowthStage(void) const = 0;
+		virtual uint16_t GetMinBuildingCapacity(void) const = 0;
+		virtual uint16_t GetMaxBuildingCapacity(void) const = 0;
 
-		virtual bool IsCompatibleWithWater(void) = 0;
-		virtual bool IsCompatibleWithZoneType(cISC4ZoneManager::ZoneType eType) = 0;
-		virtual bool IsCompatibleWithWealthType(cISC4BuildingOccupant::WealthType eType) = 0;
-		virtual bool IsCompatibleWithPurposeType(cISC4BuildingOccupant::PurposeType eType) = 0;
-		virtual bool IsCompatibleWithStyleType(uint32_t dwType) = 0;
-		virtual bool IsCompatibleWithMonopolyPieceUse(void) = 0;
-		virtual bool IsDoConstruction(void) = 0;
+		virtual bool IsCompatibleWithWater(void) const = 0;
+		virtual bool IsCompatibleWithZoneType(cISC4ZoneManager::ZoneType eType) const = 0;
+		virtual bool IsCompatibleWithWealthType(cISC4BuildingOccupant::WealthType eType) const = 0;
+		virtual bool IsCompatibleWithPurposeType(cISC4BuildingOccupant::PurposeType eType) const = 0;
+		virtual bool IsCompatibleWithStyleType(uint32_t dwType) const = 0;
+		virtual bool IsCompatibleWithMonopolyPieceUse(void) const = 0;
+		virtual bool IsDoConstruction(void) const = 0;
 
-		virtual bool GetRequiredRoads(SC4Vector<bool>& sRequiredVector) = 0;
+		virtual bool GetRequiredRoads(SC4Vector<bool>& sRequiredVector) const = 0;
 
 		virtual uint32_t GetLotConfigurationObjectCount(void) = 0;
 		virtual SC4Vector<cSC4LotConfigurationObject>* GetLotConfigurationObjectArray(void) = 0;
