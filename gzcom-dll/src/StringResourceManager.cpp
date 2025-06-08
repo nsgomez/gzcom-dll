47504a6866e1339d7e2a350f393ba44b47dd30a3
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-06-08 03:56:40 -0600

    Update StringResourceManager
    Added GetLocalizedString and GetString overloads that allow the caller
    to pass their own cIGZLanguageManager and cIGZPersistResourceManager
    instances.
    Changed the method signatures to take cRZAutoRefCount<cIGZString> by
    reference instead of a cIGZString double pointer.

--- gzcom-dll/src/StringResourceManager.cpp
+++ gzcom-dll/src/StringResourceManager.cpp
@@ -8,77 +8,82 @@
 
 namespace
 {
-	bool TryGetResourceString(uint32_t groupID, uint32_t instanceID, cIGZString** outString)
+	bool TryGetResourceString(
+		cIGZPersistResourceManager& resourceManager,
+		uint32_t groupID,
+		uint32_t instanceID,
+		cRZAutoRefCount<cIGZString>& outString)
 	{
-		bool result = false;
+		constexpr uint32_t LTEXTTypeID = 0x2026960B;
 
-		if (outString)
-		{
-			if (*outString)
-			{
-				(*outString)->Release();
-				*outString = nullptr;
-			}
+		cGZPersistResourceKey key(LTEXTTypeID, groupID, instanceID);
 
-			constexpr uint32_t LTEXTTypeID = 0x2026960B;
-
-			cGZPersistResourceKey key(LTEXTTypeID, groupID, instanceID);
-
-			cIGZPersistResourceManagerPtr resourceManager;
-			if (resourceManager)
-			{
-				// GetPrivateResource skips adding the value to the game's resource cache.
-				result = resourceManager->GetPrivateResource(
-					key,
-					GZIID_cIGZString,
-					reinterpret_cast<void**>(outString),
-					0,
-					nullptr);
-			}
-		}
-
-		return result;
+		// GetPrivateResource skips adding the value to the game's resource cache.
+		return resourceManager.GetPrivateResource(
+			key,
+			GZIID_cIGZString,
+			outString.AsPPVoid(),
+			0,
+			nullptr);
 	}
 }
 
-bool StringResourceManager::GetLocalizedString(const StringResourceKey& key, cIGZString** outString)
+bool StringResourceManager::GetLocalizedString(const StringResourceKey& key, cRZAutoRefCount<cIGZString>& outString)
+{
+	cIGZLanguageManagerPtr pLM;
+	cIGZPersistResourceManagerPtr pRM;
+
+	return GetLocalizedString(pLM, pRM, key, outString);
+}
+
+bool StringResourceManager::GetLocalizedString(
+	cIGZLanguageManager* pLM,
+	cIGZPersistResourceManager* pRM,
+	const StringResourceKey& key,
+	cRZAutoRefCount<cIGZString>& outString)
 {
 	bool result = false;
 
-	if (key.groupID != 0 && key.instanceID != 0)
+	if (key.groupID != 0 && key.instanceID != 0 && pLM && pRM)
 	{
-		cIGZLanguageManagerPtr languageManager;
-		if (languageManager)
+		// The localized resources use a group ID that is offset from the default language
+		// group ID. This system allows a single DAT file to contain string resources for all
+		// of the languages that are supported by the game.
+
+		const uint32_t currentLanguage = pLM->GetCurrentLanguage();
+		const uint32_t currentLanguageGroupID = key.groupID + currentLanguage;
+
+		// We will search the loaded string resources for a matching value in
+		// the game's currently configured language. If one is not found we will use
+		// the default string resource.
+
+		result = TryGetResourceString(*pRM, currentLanguageGroupID, key.instanceID, outString);
+		if (!result)
 		{
-			// The localized resources use a group ID that is offset from the default language
-			// group ID. This system allows a single DAT file to contain string resources for all
-			// of the languages that are supported by the game.
-
-			const uint32_t currentLanguage = languageManager->GetCurrentLanguage();
-			const uint32_t currentLanguageGroupID = key.groupID + currentLanguage;
-
-			// We will search the loaded string resources for a matching value in
-			// the game's currently configured language. If one is not found we will use
-			// the default string resource.
-
-			result = TryGetResourceString(currentLanguageGroupID, key.instanceID, outString);
-			if (!result)
-			{
-				result = TryGetResourceString(key.groupID, key.instanceID, outString);
-			}
+			result = TryGetResourceString(*pRM, key.groupID, key.instanceID, outString);
 		}
 	}
 
 	return result;
 }
 
-bool StringResourceManager::GetString(const StringResourceKey& key, cIGZString** outString)
+bool StringResourceManager::GetString(const StringResourceKey& key, cRZAutoRefCount<cIGZString>& outString)
+{
+	cIGZPersistResourceManagerPtr pRM;
+
+	return GetString(pRM, key, outString);
+}
+
+bool StringResourceManager::GetString(
+	cIGZPersistResourceManager* pRM,
+	const StringResourceKey& key,
+	cRZAutoRefCount<cIGZString>& outString)
 {
 	bool result = false;
 
-	if (key.groupID != 0 && key.instanceID != 0)
+	if (key.groupID != 0 && key.instanceID != 0 && pRM)
 	{
-		result = TryGetResourceString(key.groupID, key.instanceID, outString);
+		result = TryGetResourceString(*pRM, key.groupID, key.instanceID, outString);
 	}
 
 	return result;
