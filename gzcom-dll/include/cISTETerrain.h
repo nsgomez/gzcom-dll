bdd5ac9d19fe9d0e5b28c3839f53b98e831849e7
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-09-06 14:05:46 -0600

    Add cISTETerrainMap

--- gzcom-dll/include/cISTETerrain.h
+++ gzcom-dll/include/cISTETerrain.h
@@ -1,13 +1,11 @@
 #pragma once
-#include "cIGZUnknown.h"
+#include "cISTETerrainMap.h"
 
 class cIGZPersistDBSegment;
 class cISTETerrainView;
-class cS3DVector3;
 template<typename T> class SC4Point;
-template<typename T> class SC4Rect;
 
-class cISTETerrain : public cIGZUnknown
+class cISTETerrain : public cISTETerrainMap
 {
 public:
 	enum class eBrushTypes : int32_t
@@ -45,50 +43,6 @@ public:
 		uint32_t unknown9;
 		uint32_t unknown10;
 	};
-
-	virtual bool Init() = 0;
-	virtual bool Shutdown() = 0;
-	virtual bool IsInitialized() = 0;
-
-	virtual uint32_t CellWidth() = 0;
-	virtual uint32_t CellCountX() = 0;
-	virtual uint32_t CellCountZ() = 0;
-
-	virtual int32_t GetVertIndex(int32_t x, int32_t z) const = 0;
-	virtual void GetVertex(int32_t index, int32_t& x, int32_t& z) const = 0;
-	virtual bool LocationIsInBounds(float x, float z) const = 0;
-
-	virtual float GetAltitude(float x, float z) = 0;
-	virtual float GetAltitude(int32_t index) = 0;
-	virtual float GetAltitudeAtVertex(int32_t x, int32_t z) = 0;
-	virtual void GetAltitudes(SC4Rect<int32_t> const& unknown1, float* unknown2, int32_t unknown3, int32_t unknown4) = 0;
-	virtual void GetWaterDepths(SC4Rect<int32_t> const& unknown1, float* unknown2, int32_t unknown3, int32_t unknown4) = 0;
-	virtual void GetCellCornerAltitudes(uint32_t unknown1, uint32_t unknown2, float* unknown3) = 0;
-	virtual float GetMaxSurfaceLevel(uint32_t x, uint32_t z) = 0;
-	virtual float GetAltitudeAtNearestGrid(float x, float z) = 0;
-	virtual float GetAltitudeCheckSum(SC4Rect<int32_t> const& unknown1) const = 0;
-
-	virtual float MinHeight() = 0;
-	virtual float MaxHeight() = 0;
-	virtual float MinAllowedHeight() = 0;
-	virtual float MaxAllowedHeight() = 0;
-
-	virtual int32_t GetTerrainGridIndex(float unknown1, float unknown2) = 0;
-	virtual void GetFaceNormal(float unknown1, float unknown2, cS3DVector3& unknown3) = 0;
-	virtual void GetLerpedNormal(float unknown1, float unknown2, cS3DVector3& unknown3) = 0;
-	virtual void GetVertexNormal(uint32_t unknown1, uint32_t unknown2, cS3DVector3& unknown3) = 0;
-
-	virtual float GetSeaLevel() const = 0;
-	virtual float GetWaterLevel(int32_t index) = 0;
-	virtual float GetWaterLevel(float unknown1, float unknown2) = 0;
-	virtual float GetLerpedWaterLevel(float unknown1, float unknown2) = 0;
-	virtual float GetWaterDepthAtVertex(int32_t unknown1, int32_t unknown2) = 0;
-	virtual bool CellHasWater(uint32_t unknown1, uint32_t unknown2) const = 0;
-	virtual bool CellHasLand(uint32_t unknown1, uint32_t unknown2) const = 0;
-	virtual bool IsLandVertex(uint32_t unknown1, uint32_t unknown2) const = 0;
-	virtual bool IsWaterVertex(uint32_t unknown1, uint32_t unknown2) const = 0;
-	virtual bool IsSeaVertex(uint32_t unknown1, uint32_t unknown2) const = 0;
-	virtual bool AreSeaVertices(SC4Rect<int32_t> const& unknown1) const = 0;
 
 	virtual void DoPostCityInit() = 0;
 	virtual bool SetTerrainType(int32_t unknown1) = 0;
