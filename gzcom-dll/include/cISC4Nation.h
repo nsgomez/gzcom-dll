311521a3c6b0d796113c744be64f23ab32e54ad2
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2026-05-28 15:18:15 -0600

    Update cISC4Nation
    Fixed a few return values and marked several methods as const.

--- gzcom-dll/include/cISC4Nation.h
+++ gzcom-dll/include/cISC4Nation.h
@@ -4,7 +4,7 @@
  * cISC4Nation.h
  *
  * Copyright (C) 2016 Nelson Gomez
- * Copyright (C) 2024 Nicholas Hayes
+ * Copyright (C) 2024, 2026 Nicholas Hayes
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -34,16 +34,16 @@ class cISC4Nation : public cIGZUnknown
 		virtual bool Shutdown(void) = 0;
 
 		virtual SC4String* GetRegionDirectory(void) = 0;
-		virtual int GetRegionCount(void) = 0;
+		virtual int32_t GetRegionCount(void) const = 0;
 
-		virtual int GetActiveRegionIndex(void) = 0;
-		virtual cISC4Region* GetActiveRegion(void) = 0;
-		virtual cISC4Region* SetActiveRegion(cISC4Region* pRegion) = 0;
+		virtual int32_t GetActiveRegionIndex(void) const = 0;
+		virtual cISC4Region* GetActiveRegion(void) const = 0;
+		virtual void SetActiveRegion(cISC4Region* pRegion) = 0;
 
 		virtual bool GetIndexOfCurrentRegionSettingInPreferences(int32_t& nIndexOut) = 0;
 
 		virtual cISC4Region* GetRegion(uint32_t dwIndex) = 0;
-		virtual intptr_t DeleteRegion(uint32_t dwIndex) = 0;
+		virtual void DeleteRegion(uint32_t dwIndex) = 0;
 		virtual int32_t AddNewRegion(const cIGZString& szName) = 0;
 
 		virtual bool ResetTutorialCities(void) = 0;
