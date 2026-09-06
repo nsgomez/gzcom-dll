9626517a94eef5511aa43c1f8cae52e8180cf429
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2026-09-05 20:51:09 -0600

    Update cISC4MySimAgentSimulator::MoveIn
    The first parameter is cRZString in the game code, so it must be
    SC4String in our code.

--- gzcom-dll/include/cISC4MySimAgentSimulator.h
+++ gzcom-dll/include/cISC4MySimAgentSimulator.h
@@ -3,7 +3,7 @@
  *
  * cISC4MySimAgentSimulator.h
  *
- * Copyright (C) 2025 Nicholas Hayes
+ * Copyright (C) 2025, 2026 Nicholas Hayes
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -32,7 +32,7 @@ public:
 	virtual bool Shutdown() = 0;
 
 	virtual bool GetActiveMySims(SC4Vector<cISC4MySim*>& sims) = 0;
-	virtual void MoveIn(cIGZString& personalName, bool isMale, cISC4MySim::ZodiacSign sign, SC4String& bio, uint32_t bitmapID, SC4String& bitmapImagePath, uint8_t slot) = 0;
+	virtual void MoveIn(SC4String& personalName, bool isMale, cISC4MySim::ZodiacSign sign, SC4String& bio, uint32_t bitmapID, SC4String& bitmapImagePath, uint8_t slot) = 0;
 
 	virtual void TrackLocation(cISC4MySim* pSim) = 0;
 	virtual void HideLocation() = 0;
