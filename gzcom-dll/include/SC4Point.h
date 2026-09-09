b215c0985d190000bb199fcea5b6c576c580f2a6
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2026-09-09 03:49:00 -0600

    Add constructors and assignment operators to SC4Point

--- gzcom-dll/include/SC4Point.h
+++ gzcom-dll/include/SC4Point.h
@@ -4,6 +4,7 @@
  * SC4Point.h
  *
  * Copyright (C) 2016 Nelson Gomez
+ * Copyright (C) 2026 Nicholas Hayes
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,6 +26,46 @@ template<typename T>
 class SC4Point
 {
 	public:
+		SC4Point() : x(0), y(0)
+		{
+		}
+
+		SC4Point(T x, T y) : x(x), y(y)
+		{
+		}
+
+		SC4Point(SC4Point const& other)
+		{
+			this->x = other.x;
+			this->y = other.y;
+		}
+
+		SC4Point(SC4Point&& other) noexcept
+		{
+			this->x = other.x;
+			this->y = other.y;
+			other.x = 0;
+			other.y = 0;
+		}
+
+		SC4Point& operator=(SC4Point const& other)
+		{
+			this->x = other.x;
+			this->y = other.y;
+
+			return *this;
+		}
+
+		SC4Point& operator=(SC4Point&& other)
+		{
+			this->x = other.x;
+			this->y = other.y;
+			other.x = 0;
+			other.y = 0;
+
+			return *this;
+		}
+
 		T x;
 		T y;
 };
