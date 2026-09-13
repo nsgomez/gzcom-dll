56b96f0df8f36e5aac78dcbfb16a4bd44d8cbe5b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2026-09-13 07:59:52 -0600

    Add cIGZCanvasMessaging

---
+++ gzcom-dll/include/cIGZCanvasMessaging.h
@@ -0,0 +1,33 @@
+/*
+ * gzcom-dll - an open-source DLL Plugin SDK for SimCity 4
+ *
+ * cIGZCanvasMessaging.h
+ *
+ * Copyright (C) 2026 Nicholas Hayes
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, under
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "cIGZUnknown.h"
+
+static const uint32_t GZIID_cIGZCanvasMessaging = 0x3ac7a210;
+
+class cIGZMessageServer2;
+
+class cIGZCanvasMessaging : public cIGZUnknown
+{
+public:
+	virtual cIGZMessageServer2* MessageServer() const = 0;
+};
